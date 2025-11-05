31888c50ebdcd79bd19e4470c5a40d9e573649f7
Sunhokim2 <mcwright@naver.com>
2025-11-05 12:38:35 +0900

    최초 등록

---
+++ src/main/java/com/flow/file_extension_block_problem/FileExtensionBlockProblemApplication.java
@@ -0,0 +1,13 @@
+package com.flow.file_extension_block_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExtensionBlockProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileExtensionBlockProblemApplication.class, args);
+	}
+
+}
